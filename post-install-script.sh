e098edcfb5ce67c6d790a05fb00c8e1d4b2374f7
Jake <59594173+WarmEthernet@users.noreply.github.com>
2024-08-23 23:20:00 -0700

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -53,6 +53,9 @@ rm -rf yay
 # Install apps from the AUR
 yay -S --noconfirm brave-bin obmenu-generator
 
+# Set ly to start
+systemctl enable ly.service
+
 # Setup openbox
 mkdir -p ~/.config/openbox
 cp -a /etc/xdg/openbox ~/.config/
