087023363381eb921ad7b30a3f1411f8085b5edf
Jake <59594173+WarmEthernet@users.noreply.github.com>
2024-11-10 10:04:10 -0800

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Cache sudo creds
+echo "Please enter your sudo password"
+sudo -v
+
+# Set cached sudo creds timeout for the session
+sudo sh -c "echo 900 > /proc/$$/limits"
+
 # Update system
 echo "Updating system..."
 sudo pacman -Syu --noconfirm
@@ -8,7 +15,7 @@ sudo pacman -Syu --noconfirm
 echo "Installing prerequisites..."
 sudo pacman -S --needed --noconfirm base-devel git
 
-# Clone paru repository, build, and install it as 'nobody' user
+# Clone repo, build, and install paru
 echo "Installing paru AUR helper..."
 git clone https://aur.archlinux.org/paru.git
 cd paru/
@@ -17,11 +24,14 @@ cd ..
 rm -rf paru/
 echo "paru installed successfully."
 
-# Install Wayland and hyprland w/dependencies
+# Install wayland, hyprland, kitty, tofi, and sddm w/dependencies
 sudo pacman -S --needed --noconfirm wayland hyprland kitty sddm
 paru -S --noconrirm tofi
 
+# Enable sddm
 sudo systemctl enable sddm.service
+
+# Reboot
 sudo reboot now
 
 
